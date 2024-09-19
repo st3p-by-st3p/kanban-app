8089cf5229e5656b130ea321ed4487b690bccd26
Alex <alex.cswd@gmail.com>
2024-09-19 18:56:24 +0200

    making the room for expansion using 'webpack-merge' library

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given it's convenient with more complex configurations.
   entry: {
@@ -17,3 +19,11 @@ module.exports = {
   }
 };
 
+// Default configuration
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
