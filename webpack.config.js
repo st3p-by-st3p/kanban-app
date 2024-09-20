4a4d93887a6fa9e023ec6e501cae766b5f652733
Alex <alex.cswd@gmail.com>
2024-09-20 14:25:37 +0200

    configuring hot css refresh

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,17 @@ const common = {
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        // Test expects a RegExp! Note the slashes!
+        test:/\.css$/,
+        use: ['style-loader', 'css-loader'],
+        // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      }
+    ]
   }
 };
 
