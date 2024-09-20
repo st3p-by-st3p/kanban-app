adc40bc3361584be7908dfcf65d79a2027b5da87
Alex <alex.cswd@gmail.com>
2024-09-21 01:08:02 +0200

    configuring Babel for React compliation/transpiling

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,12 @@ const common = {
   entry: {
     app: PATHS.app
   },
+  // Add resolve.extensions.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without!!!
+  resolve: {
+    extensions: [' ', '.js', '.jsx']
+  },
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
@@ -26,6 +32,17 @@ const common = {
         test:/\.css$/,
         use: ['style-loader', 'css-loader'],
         // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      },
+      // Set up jsx. This accepts jsx too thanks to RegExp
+      {
+        test:/\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        use: ['babel-loader?cacheDirectory'],
+        // Parse only app files! Without this it will go through entire project.
+        // In addition to being slow, that will most likely result in an error.
         include: PATHS.app
       }
     ]
