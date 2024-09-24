076856af8dbcfa5c0d0ed5ecd575467cf71cb634
Alex <alex.cswd@gmail.com>
2024-09-24 02:38:08 +0200

    Implementing Note Drag and Drop Logic

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,7 +4,10 @@ import AltContainer from 'alt-container';
 import Lanes from './Lanes.jsx';
 import LaneActions from '../actions/LaneActions';
 import LaneStore from '../stores/LaneStore';
+import {DragDropContext} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
+@DragDropContext(HTML5Backend)
 export default class App extends React.Component {
 
   render() {
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,89 +1,42 @@
 import React from 'react';
+import { DragSource, DropTarget } from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
 
-export default class Note extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    // Track `editing` state
-    this.state = {
-      editing: false
+const noteSource = {
+  beginDrag(props) {
+    return {
+      id: props.id
     };
   }
+};
+
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const targetId = targetProps.id;
+    const sourceProps = monitor.getItem();
+    const sourceId = sourceProps.id;
+
+    if (sourceId !== targetId) {
+      targetProps.onMove({ sourceId, targetId });
+    }
+    console.log('dragging note', sourceProps, targetProps);
+  }
+};
+
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
+  connectDragSource: connect.dragSource()
+}))
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+  connectDropTarget: connect.dropTarget()
+}))
+export default class Note extends React.Component {
 
   render() {
-    // Render the component differently based on state.
-    if (this.state.editing) {
-      return this.renderEdit();
-    }
-    return this.renderNote();
+    const { connectDragSource, connectDropTarget,
+            id, onMove, ...props } = this.props;
+
+    return connectDragSource(connectDropTarget(
+      <li {...props}>{props.children}</li>
+    ));
   }
-
-  renderEdit = () => {
-    // We deal with blur and input handlers here. These map to DOM events.
-    // We also set selection to input end using a callback at a ref.
-    // It gets triggered after the component is mounted.
-    //
-    // We could also use a string reference (i.e. `ref="input"``) and
-    // then refer to the element in question later in the code. This
-    // would allow us to use the underlying DOM API through
-    // this.refs.input. This can be useful when combined with
-    // React lifecycle hooks.
-    return <input type="text"
-              ref={
-                (e) => e ? e.selectionStart = this.props.task.length : null
-              }
-              autoFocus={true}
-              defaultValue={this.props.task}
-              onBlur={this.finishEdit}
-              onKeyPress={this.checkEnter} />;
-  };
-
-  renderNote = () => {
-    const onDelete = this.props.onDelete;
-
-    // If the user clicks a normal note, trigger editing logic.
-    return (
-      <div onClick={this.edit}>
-        <span className="task">{ this.props.task }</span>
-        { onDelete ? this.renderDelete() : null }
-      </div>);
-  };
-
-  renderDelete = () => {
-    return <button className="delete-note"
-              onClick={this.props.onDelete}>x</button>;
-  };
-
-  edit = () => {
-    // Enter edit mode.
-    this.setState({
-      editing: true
-    });
-  };
-
-  checkEnter = (e) => {
-    // The user hit *enter*, let's finish up.
-    if (e.key === 'Enter') {
-      this.finishEdit(e);
-    }
-  };
-
-  finishEdit = (e) => {
-    // `Note` will trigger an optional `onEdit` callback once it
-    // has a new value. We will use this to communicate the change to
-    // `App`.
-    //
-    // A smarter way to deal with the default value would be to set
-    // it through `defaultProps`.
-    const value = e.target.value;
-    if (this.props.onEdit) {
-      this.props.onEdit(value);
-
-      // Exit edit mode.
-      this.setState({
-        editing: false
-      });
-    }
-  };
 }
